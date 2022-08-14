b274dcb3073a4c105d4be44b384d58ba81a17a0b
Amalassaffff <111196437+Amalassaffff@users.noreply.github.com>
2022-08-14 16:30:33 +0300

    Update get_started_page.dart

--- lib/get_started_page.dart
+++ lib/get_started_page.dart
@@ -12,9 +12,45 @@ class _GetStartedState extends State<GetStarted> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Text('get started page'),
-        ),
+        child:
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  //margin:EdgeInsets.only(bottom: 200.0) ,
+                    child: Image.asset("images/logo.png")),
+                SizedBox(height: 60.0,),
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Enter Your User ID",
+                      labelStyle: TextStyle(fontSize: 20)
+
+                    ),
+                  ),
+
+                ),
+                SizedBox(height: 40.0,),
+                   Container(
+                     height: 61,
+                    width: 218,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF44CBB1),borderRadius: BorderRadius.all(Radius.circular(15))),
+                      child: FlatButton(
+                      onPressed: null,
+                      child: Text('Get started',style:TextStyle(fontSize: 20,color: Colors.white)),
+
+
+                    ),
+                  ),
+
+
+              ],
+            ),
+          ),
       ),
     );
   }
